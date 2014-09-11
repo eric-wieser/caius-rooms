4b4ea279e66387e35f55857d5b9dfacf8450532b
Eric Wieser <wieser.eric@gmail.com>
2014-09-11 01:48:20 +0100

    Extract review-code to utils so it can be used for photos

--- database/migrate_reviews.py
+++ database/migrate_reviews.py
@@ -2,7 +2,6 @@ import db
 import orm
 import olddb
 import datetime
-import re
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 import migrate_utils
@@ -15,123 +14,6 @@ old_session = olddb.Session()
 count = 0
 
 new_locs = set()
-
-def try_recover_location(review):
-	staircase = review.staircase
-	loc_name = review.location
-
-	# fix K stair / K block confusion / random A staircase
-	if staircase == 'K' and loc_name == 'Harvey Court':
-		staircase = None
-		loc_name = 'K Block'
-	elif loc_name == '4 Rose Crescent':
-		staircase = None
-
-	try:
-		location = migrate_utils.get_location(new_session, review.location)
-	except migrate_utils.NewLocation as e:
-		location = e.args[1]
-		new_locs.add(location)
-
-	if staircase != 'None' and staircase:
-		try:
-			location = new_session.query(orm.Cluster).filter(
-				(orm.Cluster.parent == location) &
-				(orm.Cluster.name == staircase)
-			).one()
-		except NoResultFound:
-			print "New staircase {} in {}".format(staircase, location)
-			location = orm.Cluster(name=staircase, type="staircase", parent=location)
-			new_locs.add(location)
-
-	return location
-
-def try_recover_room(review):
-	location = try_recover_location(review)
-
-	rooms = (new_session
-		.query(orm.Room)
-		.filter(orm.Room.parent == location)
-	)
-
-	# look for an existing room
-	try:
-		return rooms.filter(orm.Room.name == review.room).one()
-	except NoResultFound:
-		pass
-
-	# look for joint rooms that share a number
-	numeric_room = review.room.rstrip('*')
-	for r in rooms:
-		s = re.split('\D+', r.name)
-		if numeric_room in s:
-			return r
-
-	# we have a new room
-	name = review.room
-	if name.endswith('**'):
-		print "Truncated starred room {} {}".format(name, location)
-		name = name.rstrip('*') + '*'
-
-	return orm.Room(
-		name=name,
-		parent=location
-	)
-
-def try_recover_ballot(year):
-	# get all ballots for the year
-	ballots = (new_session
-		.query(orm.Ballot)
-		.filter(orm.Ballot.rental_year == year)
-		.order_by(orm.Ballot.opens_at.desc())
-	)
-
-	# assume undergrad?
-	try:
-		return ballots.filter(orm.Ballot.type == 'ugrad').first()
-	except NoResultFound:
-		pass
-
-	# or not
-	try:
-		return ballots.first()
-	except NoResultFound:
-		pass
-
-	# or roll a new one
-	return orm.Ballot(
-		is_reconstructed=True,
-
-		type='ugrad',
-		opens_at=datetime.date(year + 1, 1, 1)
-	)
-
-
-def try_recover_listing(room, ts):
-	if ts.month > 9:
-		guessed_ballot_year = ts.year
-	else:
-		guessed_ballot_year = ts.year - 1
-
-	# see if we have a listing already
-	try:
-		return (new_session
-			.query(orm.RoomListing)
-			.join(orm.Ballot)
-			.filter(orm.RoomListing.room == room)
-			.filter(orm.Ballot.rental_year == guessed_ballot_year)
-		).one()
-	except NoResultFound:
-		pass
-
-	# get all ballots for the year
-	ballot = try_recover_ballot(guessed_ballot_year)
-
-	return orm.RoomListing(
-		room=room,
-		ballot=ballot
-	)
-
 
 seen_ts = set()
 duplicates = 0
@@ -152,9 +34,12 @@ for old_review in old_session.query(olddb.orm.accom_guide_input).order_by(olddb.
 	seen_ts.add(ts)
 
 	if old_review.roomID == 0:
-		room = try_recover_room(old_review)
-		if not room:
-			continue
+		location = migrate_utils.get_location_with_stair(
+			new_session, old_review.location, old_review.staircase
+		)
+		room = migrate_utils.try_recover_room(
+			new_session, location, old_review.room
+		)
 	else:
 		try:
 			room = new_session.query(orm.Room).filter(orm.Room.id == old_review.roomID).one()
@@ -190,7 +75,7 @@ for old_review in old_session.query(olddb.orm.accom_guide_input).order_by(olddb.
 		room.bedroom_view = bedroom_view
 
 
-	listing = try_recover_listing(room, ts)
+	listing = migrate_utils.try_recover_listing(new_session, room, ts)
 	if listing.occupancies:
 		occupancy = listing.occupancies[0]
 	else:
--- database/migrate_utils.py
+++ database/migrate_utils.py
@@ -1,8 +1,10 @@
-import db
-import orm
+import re
 
 from sqlalchemy.orm import aliased
 from sqlalchemy.orm.exc import NoResultFound
+
+import db
+import orm
 
 class NewLocation(Exception):
 	pass
@@ -35,6 +37,118 @@ def get_location(new_session, loc_name):
 				parent=root
 			)
 		))
+
+def get_location_with_stair(new_session, loc_name, staircase):
+	# fix K stair / K block confusion / random A staircase
+	if staircase == 'K' and loc_name == 'Harvey Court':
+		staircase = None
+		loc_name = 'K Block'
+	elif loc_name == '4 Rose Crescent':
+		staircase = None
+
+	try:
+		location = get_location(new_session, loc_name)
+	except NewLocation as e:
+		location = e.args[1]
+		new_locs.add(location)
+
+	if staircase != 'None' and staircase:
+		try:
+			location = new_session.query(orm.Cluster).filter(
+				(orm.Cluster.parent == location) &
+				(orm.Cluster.name == staircase)
+			).one()
+		except NoResultFound:
+			print "New staircase {} in {}".format(staircase, location)
+			location = orm.Cluster(name=staircase, type="staircase", parent=location)
+			new_locs.add(location)
+
+	return location
+
+
+def try_recover_room(new_session, location, name):
+	rooms = (new_session
+		.query(orm.Room)
+		.filter(orm.Room.parent == location)
+	)
+
+	# look for an existing room
+	try:
+		return rooms.filter(orm.Room.name == name).one()
+	except NoResultFound:
+		pass
+
+	# look for joint rooms that share a number
+	numeric_room = name.rstrip('*')
+	for r in rooms:
+		s = re.split('\D+', r.name)
+		if numeric_room in s:
+			return r
+
+	# we have a new room
+	name = name
+	if name.endswith('**'):
+		print "Truncated starred room {} {}".format(name, location)
+		name = name.rstrip('*') + '*'
+
+	return orm.Room(
+		name=name,
+		parent=location
+	)
+
+def try_recover_ballot(new_session, year):
+	# get all ballots for the year
+	ballots = (new_session
+		.query(orm.Ballot)
+		.filter(orm.Ballot.rental_year == year)
+		.order_by(orm.Ballot.opens_at.desc())
+	)
+
+	# assume undergrad?
+	try:
+		return ballots.filter(orm.Ballot.type == 'ugrad').first()
+	except NoResultFound:
+		pass
+
+	# or not
+	try:
+		return ballots.first()
+	except NoResultFound:
+		pass
+
+	# or roll a new one
+	return orm.Ballot(
+		is_reconstructed=True,
+
+		type='ugrad',
+		opens_at=datetime.date(year + 1, 1, 1)
+	)
+
+
+def try_recover_listing(new_session, room, ts):
+	if ts.month > 9:
+		guessed_ballot_year = ts.year
+	else:
+		guessed_ballot_year = ts.year - 1
+
+	# see if we have a listing already
+	try:
+		return (new_session
+			.query(orm.RoomListing)
+			.join(orm.Ballot)
+			.filter(orm.RoomListing.room == room)
+			.filter(orm.Ballot.rental_year == guessed_ballot_year)
+		).one()
+	except NoResultFound:
+		pass
+
+	# get all ballots for the year
+	ballot = try_recover_ballot(new_session, guessed_ballot_year)
+
+	return orm.RoomListing(
+		room=room,
+		ballot=ballot
+	)
 
 def sanitize_view(v):
 	if not v:
