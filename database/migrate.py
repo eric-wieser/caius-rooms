b1bbdd84984f54c346f845c3e691f18ea2541e91
Eric Wieser <wieser.eric@gmail.com>
2014-09-11 01:18:59 +0100

    Top-level migration script. Yes, I should probably not have these run on import...

---
+++ database/migrate.py
@@ -0,0 +1,20 @@
+import init_db
+print "init_db: Done"
+
+import migrate_users
+print "migrate_users: Done"
+
+import migrate_ballots
+print "migrate_ballots: Done"
+
+import migrate_rooms
+print "migrate_rooms: Done"
+
+import migrate_room_listings
+print "migrate_room_listings: Done"
+
+import migrate_occupancies
+print "migrate_occupancies: Done"
+
+import migrate_reviews
+print "migrate_reviews: Done"
