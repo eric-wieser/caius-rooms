c3f22eb0b945e5031707b944425461046892da6d
Eric Wieser <wieser.eric@gmail.com>
2014-10-03 00:01:27 +0100

    Make server migration use mysql db

--- database/olddb.py
+++ database/olddb.py
@@ -2,16 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 
-import os
 
+try:
+	from db_conn import user, password
+	engine = create_engine(
+		'mysql+mysqldb://{user}:{password}@localhost/gcsu?charset=latin-1&use_unicode=0'.format(
+			user=user, password=password
+		),
+		pool_recycle=3600
+	)
+except ImportError:
+	import os
+	base_dir = os.path.dirname(__file__)
+	engine = create_engine('sqlite:///{}'.format(os.path.join(base_dir, 'srcf.db')))
 
-Session = sessionmaker()
-
-base_dir = os.path.dirname(__file__)
-engine = create_engine('sqlite:///{}'.format(os.path.join(base_dir, 'srcf.db')))
-
-Session.configure(bind=engine)
-
+Session = sessionmaker(engine)
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
