b0834ee0824caa2dcccdda77eecd95433cb82eb7
Eric Wieser <wieser.eric@gmail.com>
2014-11-03 16:28:52 +0000

    spaces -> tabs

--- database/db.py
+++ database/db.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import sessionmaker
 try:
 	from db_conn import user, password
 	engine = create_engine(
-        'mysql+mysqldb://{user}:{password}@localhost/gcsu/roompicks?charset=utf8&use_unicode=1'.format(
-        	user=user, password=password
-        ),
-        pool_recycle=3600
+		'mysql+mysqldb://{user}:{password}@localhost/gcsu/roompicks?charset=utf8&use_unicode=1'.format(
+			user=user, password=password
+		),
+		pool_recycle=3600
 	)
 except ImportError:
 	print "Using development sqlite database"
