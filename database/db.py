673df94ca06a60b3962f8f28e377893074ea44bf
Eric Wieser <wieser.eric@gmail.com>
2014-12-27 17:11:43 +0000

    Force SQLite to respect foreign keys
    This makes the dev behaviour match production

--- database/db.py
+++ database/db.py
@@ -16,4 +16,13 @@ except ImportError:
 	base_dir = os.path.dirname(__file__)
 	engine = create_engine('sqlite:///{}'.format(os.path.join(base_dir, 'dev.db')))
 
+	from sqlalchemy.engine import Engine
+	from sqlalchemy import event
+
+	@event.listens_for(Engine, "connect")
+	def set_sqlite_pragma(dbapi_connection, connection_record):
+	    cursor = dbapi_connection.cursor()
+	    cursor.execute("PRAGMA foreign_keys=ON")
+	    cursor.close()
+
 Session = sessionmaker(engine)
